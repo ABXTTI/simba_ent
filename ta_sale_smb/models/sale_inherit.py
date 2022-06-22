04cd2fd0a4f32bb0e26512690a323bf67a3b4cfb
ABXTTI <49796896+ABXTTI@users.noreply.github.com>
2022-06-23 03:59:44 +0500

    Add files via upload

--- ta_sale_smb/models/sale_inherit.py
+++ ta_sale_smb/models/sale_inherit.py
@@ -10,16 +10,29 @@ class SaleOrder(models.Model):
         order_line = self.order_line
         total_advance = 0
         commission = 0
+        commission_product_id = self.env['product.template'].search(
+            [('name', '=', 'Commission'), ('is_commission_product', '=', True)])
         for rec in order_line:
             if rec.product_id.is_advance_product:
-                total_advance += (rec.product_uom_qty * rec.price_unit * rec.advance_in_percentage)
-                if total_advance and self.partner_id.default_advance_in_percentage and self.partner_id.default_commission_percentage:
-                    commission = total_advance * self.partner_id.default_commission_percentage
-                    commission_product_id = self.env['product.template'].search([('name', '=', 'Commission'),('is_commission_product', '=', True)])
+                total_advance += (rec.product_uom_qty * rec.price_unit * rec.advance_in_percentage / 100)
+
+                if total_advance and self.partner_id.default_advance_in_percentage:
+                    commission = total_advance * self.partner_id.default_commission_percentage / 100
                     if len(commission_product_id) > 1:
                         raise odoo.exceptions.ValidationError("Commission Product Cannot be more than one !!!!!!!")
                     elif not commission_product_id:
                         raise odoo.exceptions.ValidationError("Create 1 Commission Product !!!!!")
+
+
+        commission_line_exist = order_line.search([('product_id', '=', commission_product_id.id),('order_id', '=', self.id)])
+        if not commission_line_exist and commission:
+            order_line.create({
+                'product_id': commission_product_id.id,
+                'commission_percentage': self.partner_id.default_commission_percentage,
+                'product_uom_qty': 1,
+                'price_unit': commission,
+                'order_id': self.id,
+            })
         return rtn
 
 class SaleOrderLine(models.Model):
@@ -27,11 +40,28 @@ class SaleOrderLine(models.Model):
 
     advance_in_percentage = fields.Float(string="Advance in %")
     commission_percentage = fields.Float(string="Commission in %")
+    is_commission_product = fields.Boolean(string="IS Commission Product", related="product_id.is_commission_product")
+    is_advance_product = fields.Boolean(string="IS Commission Product", related="product_id.is_advance_product")
 
     @api.onchange('product_id')
     def get_values_onchange_product(self):
         for rec in self:
             if rec.product_id.is_commission_product:
                 rec.commission_percentage = rec.order_partner_id.default_commission_percentage
-            if rec.product_id.is_advance_product:
+            elif rec.product_id.is_advance_product:
                 rec.advance_in_percentage = rec.order_partner_id.default_advance_in_percentage
+
+    @api.onchange('commission_percentage')
+    def onchange_commission_percentage(self):
+        for rec in self:
+            total_advance = 0
+            commission = 0
+            if rec.product_id.is_commission_product:
+                order_line = rec.order_id.order_line
+                for line in order_line:
+                    if line.product_id.is_advance_product:
+                        total_advance += (line.product_uom_qty * line.price_unit * line.advance_in_percentage / 100)
+                        if total_advance:
+                            commission = total_advance * rec.commission_percentage / 100
+
+                rec.price_unit = commission
